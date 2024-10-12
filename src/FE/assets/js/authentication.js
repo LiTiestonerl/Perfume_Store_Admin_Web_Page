ae597956bf5695a682ef7112bc99da3b7f5d0a89
LiTiestonerl <cunconcut394@gmail.com>
2024-10-12 17:00:33 +0700

    Update_12-10-2024

---
+++ src/FE/assets/js/authentication.js
@@ -0,0 +1,43 @@
+const BASE_API_URL = "http://localhost:5177/api/auth";
+
+document.getElementById("loginButton").addEventListener("click", loginWithGoogle);
+
+function loginWithGoogle() {
+  window.location.href = `${BASE_API_URL}/signin-google`;
+}
+
+// Xử lý phản hồi từ Google để nhận JWT token
+async function handleGoogleResponse() {
+  try {
+    const response = await fetch(`${BASE_API_URL}/google-response`);
+
+    if (!response.ok) {
+      throw new Error("Authentication failed");
+    }
+
+    const data = await response.json();
+
+    if (data.token) {
+      localStorage.setItem("jwtToken", data.token);
+      console.log("Đăng nhập thành công!");
+      console.log("Thông tin người dùng:", data);
+
+      window.location.href = '/index.html';
+    } else {
+      console.error("Token không tồn tại");
+    }
+  } catch (error) {
+    console.error("Lỗi:", error);
+  }
+}
+
+if (window.location.pathname.includes('google-response')) {
+  handleGoogleResponse();
+}
+
+document.getElementById("logout").addEventListener("click", logout);
+function logout() {
+  localStorage.removeItem("jwtToken");
+  console.log("Đã đăng xuất");
+  window.location.href = '/login.html';
+}
